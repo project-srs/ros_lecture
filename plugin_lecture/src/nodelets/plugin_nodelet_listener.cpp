3eab24468251b401ea337a2c1b4ef66e4cec864f
project-srs <shirasato.robot.seisakujo@gmail.com>
2019-04-29 22:05:55 +0900

    plugin_lecture: add nodelet

---
+++ plugin_lecture/src/nodelets/plugin_nodelet_listener.cpp
@@ -0,0 +1,37 @@
+#include <ros/ros.h>
+#include <nodelet/nodelet.h>
+#include <pluginlib/class_list_macros.h>
+#include <std_msgs/String.h>
+
+namespace plugin_lecture{
+class plugin_nodelet_listener : public nodelet::Nodelet{
+public:
+  plugin_nodelet_listener();
+  ~plugin_nodelet_listener();
+  virtual void onInit();
+  void chatter_callback(const std_msgs::String& string_msg);
+private:
+  ros::NodeHandle nh_;
+  ros::Subscriber sub_;
+  ros::Timer timer_;
+};
+
+plugin_nodelet_listener::plugin_nodelet_listener(){
+  NODELET_INFO("Listener Constructor");
+}
+
+plugin_nodelet_listener::~plugin_nodelet_listener(){
+  NODELET_INFO("Listener Destructor");
+}
+
+void plugin_nodelet_listener::onInit(){
+  NODELET_INFO("Listener Init");
+  nh_ = getNodeHandle();
+  sub_ = nh_.subscribe("chatter", 10, &plugin_nodelet_listener::chatter_callback, this);
+}
+
+void plugin_nodelet_listener::chatter_callback(const std_msgs::String& string_msg){
+  NODELET_INFO("recieve: %s", string_msg.data.c_str());
+}
+} // namespace plugin_lecture
+PLUGINLIB_EXPORT_CLASS(plugin_lecture::plugin_nodelet_listener, nodelet::Nodelet)
---
+++ plugin_lecture/src/nodelets/plugin_nodelet_talker.cpp
@@ -0,0 +1,47 @@
+#include <ros/ros.h>
+#include <nodelet/nodelet.h>
+#include <pluginlib/class_list_macros.h>
+#include <std_msgs/String.h>
+
+namespace plugin_lecture{
+class plugin_nodelet_talker : public nodelet::Nodelet{
+public:
+  plugin_nodelet_talker();
+  ~plugin_nodelet_talker();
+  virtual void onInit();
+  void timer_callback(const ros::TimerEvent&);
+private:
+  ros::NodeHandle nh_;
+  ros::NodeHandle pnh_;
+  ros::Publisher pub_;
+  ros::Timer timer_;
+  std::string content_;
+};
+
+plugin_nodelet_talker::plugin_nodelet_talker(){
+  NODELET_INFO("Talker Constructor");
+}
+
+plugin_nodelet_talker::~plugin_nodelet_talker(){
+  NODELET_INFO("Talker Destructor");
+}
+
+void plugin_nodelet_talker::onInit(){
+  NODELET_INFO("Talker Init");
+  nh_ = getNodeHandle();
+  pnh_ = getPrivateNodeHandle();
+
+  content_ = "hello";
+  pnh_.getParam("content", content_);
+  pub_ = nh_.advertise<std_msgs::String>("chatter", 10);
+  timer_ = nh_.createTimer(ros::Duration(1.0), &plugin_nodelet_talker::timer_callback, this);
+}
+
+void plugin_nodelet_talker::timer_callback(const ros::TimerEvent&){
+  NODELET_INFO("send: %s", content_.c_str());
+  std_msgs::String string_msg;
+  string_msg.data = content_;
+  pub_.publish(string_msg);
+}
+} // namespace plugin_lecture
+PLUGINLIB_EXPORT_CLASS(plugin_lecture::plugin_nodelet_talker, nodelet::Nodelet)
