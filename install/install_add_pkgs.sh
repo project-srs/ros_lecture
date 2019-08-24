cf7bd2845d4c1c40e308429efe39564ee68b2c0c
project-srs <shirasato.robot.seisakujo@gmail.com>
2019-08-24 16:16:41 +0900

    install: first commit

---
+++ install/install_add_pkgs.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# common
+sudo apt install ros-kinetic-jsk-visualization 
+sudo apt install ros-kinetic-robot-pose-publisher 
+sudo apt install ros-kinetic-joystick-drivers 
+
+# sim
+sudo apt install ros-kinetic-gazebo-ros
+sudo apt install ros-kinetic-gazebo-ros-control 
+sudo apt install ros-kinetic-ros-control
+sudo apt install ros-kinetic-ros-controllers 
