84f7d9d8512a017ca56bc3979f2d8fda6445d2c1
project-srs <project@srs>
2022-01-10 14:45:29 -0800

    update

--- adv_lecture/src/adv_orientation.cpp
+++ adv_lecture/src/adv_orientation.cpp
@@ -11,10 +11,10 @@
 5. rotate matrix: geometry_msgs::mat3
 */
 
-void tf_quat_to_rpy(double& roll, double& pitch, double& yaw, tf::Quaternion quat){
+void tf_quat_to_rpy(tf::Quaternion quat, double& roll, double& pitch, double& yaw){
 	tf::Matrix3x3(quat).getRPY(roll, pitch, yaw);  //rpy are Pass by Reference
 }
-void geometry_quat_to_rpy(double& roll, double& pitch, double& yaw, geometry_msgs::Quaternion geometry_quat){
+void geometry_quat_to_rpy(geometry_msgs::Quaternion geometry_quat, double& roll, double& pitch, double& yaw){
 	tf::Quaternion quat;
 	quaternionMsgToTF(geometry_quat, quat);
 	tf::Matrix3x3(quat).getRPY(roll, pitch, yaw);  //rpy are Pass by Reference
@@ -28,6 +28,39 @@ geometry_msgs::Quaternion rpy_to_geometry_quat(double roll, double pitch, double
 	quaternionTFToMsg(quat, geometry_quat);
 	return geometry_quat;
 }
+
+void calc_quat_to_rpy(geometry_msgs::Quaternion geometry_quat, double& roll, double& pitch, double& yaw){
+	float q0q0 = geometry_quat.w * geometry_quat.w;
+	float q1q1 = geometry_quat.x * geometry_quat.x;
+	float q2q2 = geometry_quat.y * geometry_quat.y;
+	float q3q3 = geometry_quat.z * geometry_quat.z;
+	float q0q1 = geometry_quat.w * geometry_quat.x;
+	float q0q2 = geometry_quat.w * geometry_quat.y;
+	float q0q3 = geometry_quat.w * geometry_quat.z;
+	float q1q2 = geometry_quat.x * geometry_quat.y;
+	float q1q3 = geometry_quat.x * geometry_quat.z;
+	float q2q3 = geometry_quat.y * geometry_quat.z;
+
+    roll = atan2f((2.f * (q2q3 + q0q1)), (q0q0 - q1q1 - q2q2 + q3q3));
+    pitch = -asinf((2.f * (q1q3 - q0q2)));
+    yaw = atan2f((2.f * (q1q2 + q0q3)), (q0q0 + q1q1 - q2q2 - q3q3));
+}
+geometry_msgs::Quaternion calc_rpy_to_quat(double roll, double pitch, double yaw){
+    double cy = cos(yaw * 0.5);
+    double sy = sin(yaw * 0.5);
+    double cp = cos(pitch * 0.5);
+    double sp = sin(pitch * 0.5);
+    double cr = cos(roll * 0.5);
+    double sr = sin(roll * 0.5);
+
+    geometry_msgs::Quaternion q;
+    q.w = cr * cp * cy + sr * sp * sy;
+    q.x = sr * cp * cy - cr * sp * sy;
+    q.y = cr * sp * cy + sr * cp * sy;
+    q.z = cr * cp * sy - sr * sp * cy;
+	return q;
+}
+
 int main(int argc, char **argv){
 	printf("start\n");
 	double roll, pitch, yaw;
@@ -36,7 +69,7 @@ int main(int argc, char **argv){
 	
 	printf("\ntf_quat to rpy\n");
 	quat=tf::Quaternion(0.707,0,0,0.707);
-	tf_quat_to_rpy(roll, pitch, yaw, quat);
+	tf_quat_to_rpy(quat, roll, pitch, yaw);
 	printf("input:  tf_Quat x:%f, y:%f, z:%f, w:%f\n", quat.getAxis().x(), quat.getAxis().y(), quat.getAxis().z(), quat.getW());
 	printf("output: rpy roll:%f, pitch:%f, yaw:%f\n", roll, pitch, yaw);
 
@@ -45,7 +78,7 @@ int main(int argc, char **argv){
 	geometry_quat.y=0;
 	geometry_quat.z=0;
 	geometry_quat.w=0;
-	geometry_quat_to_rpy(roll, pitch, yaw, geometry_quat);
+	geometry_quat_to_rpy(geometry_quat, roll, pitch, yaw);
 	printf("input:  geoQuat x:%f, y:%f, z:%f, w:%f\n", geometry_quat.x, geometry_quat.y, geometry_quat.z, geometry_quat.w);
 	printf("output: rpy roll:%f, pitch:%f, yaw:%f\n", roll, pitch, yaw);
 
@@ -65,6 +98,6 @@ int main(int argc, char **argv){
 	printf("input:  rpy roll:%f, pitch:%f, yaw:%f\n", roll, pitch, yaw);
 	printf("output: geoQuat x:%f, y:%f, z:%f, w:%f\n", geometry_quat.x, geometry_quat.y, geometry_quat.z, geometry_quat.w);
 
- 	return 0;
+	return 0;
 }
 
--- basic_lecture/src/basic_twist_publisher.cpp
+++ basic_lecture/src/basic_twist_publisher.cpp
@@ -2,27 +2,57 @@
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Joy.h>
 
-geometry_msgs::Twist cmd_vel;
-void joy_callback(const sensor_msgs::Joy& joy_msg)
-{
-  cmd_vel.linear.x = joy_msg.axes[1];
-  cmd_vel.linear.y = joy_msg.axes[0];
-  cmd_vel.angular.z = joy_msg.axes[2];
-}
+class TwistPublisher{
+public:
+  TwistPublisher(){
+    cmd_pub_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+    joy_sub_ = nh_.subscribe("joy", 10, &TwistPublisher::joyCallback, this);
+    timer_ = nh_.createTimer(ros::Duration(0.1), &TwistPublisher::timerCallback, this);
+  }
+
+  void joyCallback(const sensor_msgs::Joy& joy_msg) {
+    last_joy_ = joy_msg;
+  }
+
+  void timerCallback(const ros::TimerEvent& e) {
+    int assign_x = 1;
+    int assign_y = 0;
+    int assign_z = 3;
+    nh_.getParam("assign_x", assign_x);
+    nh_.getParam("assign_y", assign_y);
+    nh_.getParam("assign_z", assign_z);
+
+    float max_x = 0.5;
+    float max_y = 0.5;
+    float max_z = 1.5;
+    nh_.getParam("max_x", max_x);
+    nh_.getParam("max_y", max_y);
+    nh_.getParam("max_z", max_z);
+
+    geometry_msgs::Twist cmd_vel;
+    if(assign_x < last_joy_.axes.size()){
+      cmd_vel.linear.x = max_x * last_joy_.axes[assign_x];
+    }
+    if(assign_y < last_joy_.axes.size()){
+      cmd_vel.linear.y = max_y * last_joy_.axes[assign_y];
+    }
+    if(assign_z < last_joy_.axes.size()){
+      cmd_vel.angular.z = max_z * last_joy_.axes[assign_z];
+    }
+    cmd_pub_.publish(cmd_vel);
+  }
+
+  ros::NodeHandle nh_;
+  ros::Publisher cmd_pub_;
+  ros::Subscriber joy_sub_;
+  ros::Timer timer_;
+  sensor_msgs::Joy last_joy_;
+};
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "basic_twist_publisher");
-  ros::NodeHandle nh;
-  ros::Publisher cmd_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
-  ros::Subscriber joy_sub = nh.subscribe("joy", 10, joy_callback);
-
-  ros::Rate loop_rate(10);
-  while (ros::ok())
-  {
-    cmd_pub.publish(cmd_vel);
-    ros::spinOnce();
-    loop_rate.sleep();
-  }
+  TwistPublisher twist_publisher;
+  ros::spin();
   return 0;
 }
