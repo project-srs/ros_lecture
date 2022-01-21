72d8aba258d3947337633a5d0dc8d10897f808b0
project-srs <project@srs>
2022-01-21 15:07:04 -0800

    modify

--- basic_lecture/src/basic_simple_talker.cpp
+++ basic_lecture/src/basic_simple_talker.cpp
@@ -1,6 +1,5 @@
 #include <ros/ros.h>
 #include <std_msgs/String.h>
-#include <ros/package.h>
 
 int main(int argc, char** argv)
 {
@@ -8,15 +7,6 @@ int main(int argc, char** argv)
   ros::NodeHandle nh;
   ros::Publisher chatter_pub = nh.advertise<std_msgs::String>("chatter", 10);
   ros::Rate loop_rate(10);
-
-  ROS_INFO("get path");
-  std::vector<std::string> model_paths;
-  ros::package::getPlugins("gazebo_ros","gazebo_model_path",model_paths);
-  for (std::vector<std::string>::iterator iter=model_paths.begin(); iter != model_paths.end(); iter++)
-  {
-    ROS_INFO("Model path %s",(*iter).c_str());
-    // gazebo::common::SystemPaths::Instance()->AddModelPaths(iter->c_str());
-  }
 
   while (ros::ok())
   {
