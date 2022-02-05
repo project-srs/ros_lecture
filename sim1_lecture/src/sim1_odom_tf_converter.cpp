48b83afdaa36b99feb7f3ff579c2b168a89ecf7a
project-srs <project@srs>
2022-02-05 04:42:00 -0800

    sim1_lecure: update

--- sim1_lecture/src/sim1_odom_tf_converter.cpp
+++ sim1_lecture/src/sim1_odom_tf_converter.cpp
@@ -7,17 +7,28 @@
 class Converter
 {
 public:
-  Converter(void)
+  Converter(void): nh_(), pnh_("~")
   {
     joy_sub_ = nh_.subscribe("/tracker", 10, &Converter::odomCallback, this);
   }
   void odomCallback(const nav_msgs::Odometry& msg)
   {
+    std::string frame_id;
+
+    std::string parent_frame_id = "";
+    pnh_.getParam("parent_frame_id", parent_frame_id);
+    if (parent_frame_id != ""){
+      frame_id = parent_frame_id;
+    } else {
+      frame_id = msg.header.frame_id;
+    }
+
     tf::Transform transform;
     tf::poseMsgToTF(msg.pose.pose, transform);
-    br_.sendTransform(tf::StampedTransform(transform, msg.header.stamp, /*msg.header.frame_id*/ "odom", msg.child_frame_id));
+    br_.sendTransform(tf::StampedTransform(transform, msg.header.stamp, frame_id, msg.child_frame_id));
   }
   ros::NodeHandle nh_;
+  ros::NodeHandle pnh_;
   ros::Subscriber joy_sub_;
   tf::TransformBroadcaster br_;
 };
